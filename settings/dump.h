f6c86880ea8a41d01ef13f73bac045478d442b7d
ichthyandr <wimax.peter@gmail.com>
2019-08-05 17:23:48 +0300

    dumping done

---
+++ settings/dump.h
@@ -0,0 +1,33 @@
+/*
+ * dump.h
+ *
+ * Created: 15.05.2019 10:03:01
+ *  Author: peter
+ */ 
+
+
+#ifndef DUMP_H_
+#define DUMP_H_
+#include <stdint.h>
+#include <stdbool.h>
+#include "settings.h"
+
+#define MANUFACTURER_ID (0x007F7F)
+#define NETWORK_NUMBER	(0x00)
+#define MODEL_NUMBER	(0x00)
+
+/**************************************************************/
+/// dump all settings
+void save_All();
+
+/**************************************************************/
+/// read SysEx message from network, parse and apply to settings
+///\param midiMsgType[in]		- message type, must be MIDI_SYSEX_END 
+///\param midiMsg[in]			- midi message
+///\param midiMsgLength[in]		- midi message length
+///\param midiBuffLength[in]	- midi buffer length
+///\return true if decoding is successful, else - false 
+bool handleMidiSysExSettings( uint8_t midiMsgType, uint8_t * midiMsg, uint16_t midiMsgLength, uint16_t midiBuffLength);
+
+
+#endif /* DUMP_H_ */
